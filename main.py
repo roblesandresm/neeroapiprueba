dc37de5e3b8fbdd097cbbf452c0b4e29d4625e10
Andres Robles <roblesandresm1@gmail.com>
2023-01-27 19:54:28 -0500

    directory structure definition

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(title="Api POS", version="0.0.1")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
